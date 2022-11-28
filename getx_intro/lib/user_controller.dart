22a93c869a7aa6208d14f3ad127b016fbcc3d025
Matheus <matheus251170@outlook.com>
2022-11-28 19:06:00 -0300

    usando observables em objetos

---
+++ getx_intro/lib/user_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+import 'package:getx_intro/user_model.dart';
+
+class MyController extends GetxController {
+  Rx<UserModel> user = UserModel().obs;
+
+  void setUsername(String username) {
+    // user.update((obj) { obj?.name = username;});
+
+    user.value.name = username;
+    user.refresh();
+  }
+
+  void setAge(int age){
+    // user.update((obj) { obj?.age = age;});
+    user.value.age = age;
+    user.refresh();
+  }
+}
