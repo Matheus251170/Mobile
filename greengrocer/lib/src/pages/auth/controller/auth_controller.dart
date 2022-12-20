986dd19247967cb075efe8b281cd6dc2c4b94481
Matheus <matheus251170@outlook.com>
2022-12-20 11:47:26 -0300

    inclui controller de autenticação

---
+++ greengrocer/lib/src/pages/auth/controller/auth_controller.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  RxBool isLoading = false.obs;
+
+  Future<void> signIn({required String email, required String password}) async {
+    isLoading.value = true;
+
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+
+    isLoading.value = false;
+  }
+
+  Future<void> signUp({required String email, required String password, required String name, required String cpf, required String cell}) async {
+    isLoading.value = true;
+
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+
+    isLoading.value = false;
+  }
+}
